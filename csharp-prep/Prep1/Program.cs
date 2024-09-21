26f7d91bc0d1d6e43d6df19d66d1624b8d67b19e
Gohouri <angejunior246@gmail.com>
2024-09-21 23:47:22 +0000

    New Update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("Hello Prep1 World!");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Hello Prep1 World! {last}, {first} {last}.");
     }
 }
